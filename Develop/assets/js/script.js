6e425161f5917825020702b19833494f9199deee
cheesecake_assassin <santasebastian@yahoo.com>
2021-12-14 13:24:18 -0500

    added input validation for length entry

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,7 +1,21 @@
 function generatePassword() {
     var passwordLength;
-    // Prompt user for password length
-    passwordLength = prompt("Please enter desired password length (8 - 128 inclusive)");
+    var validLength = false;
+
+    // While there is an valid length entered, the user will keep being prompted
+    while (!validLength) {
+        // Prompt user for password length
+        passwordLength = prompt("Please enter desired password length (8 - 128 inclusive)");
+        passwordLength = parseInt(passwordLength); // Gets integer value of string
+        
+        // Checks that length is between 8 and 128 inclusive
+        if (passwordLength >= 8 && passwordLength <= 128)
+        {
+            validLength = true;
+        } else {
+            alert("Invalid entry, try again...") // Error message
+        }
+    }
 }
 
 // Get references to the #generate element
